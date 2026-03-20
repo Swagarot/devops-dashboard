d453038d88e36646d5cb242cf3cec403edcc3fdb
Roye <ruthsus132@gmail.com>
2026-03-20 14:09:49 +0200

    minor fixes in sysinfo.py and inegration of sysinfo.py into main.py

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
-while true: 
+import sysinfo
+
+while True: 
  print("- [1] System Info")
  print("- [2] Log Checker")
  print("- [3] Task List")
  print("- [0] Exit")
  choice = int(input("select menu option"))
- match choice
+ match choice:
  
   case 1: 
-  input ("Coming soon")
+   sysinfo.show_sysinfo()
   case 2:
-  input ("Coming soon")
+   input ("Coming soon")
   case 3:
-  intput ("Coming soon")
+   intput ("Coming soon")
   case 0:
-  input ("press enter to exit")
-  break
+   input ("press enter to exit")
+   break
 
--- sysinfo.py
+++ sysinfo.py
@@ -1,10 +1,10 @@
 import os
 import getpass
 import socket
-def show_sysinfo()
+def show_sysinfo():
     hostname = socket.gethostname()
     user = getpass.getuser()
-    cwd = os.getcws()
+    cwd = os.getcwd()
     print(f"Hostname: {hostname}")
     print(f"User: {user}")
     print(f"Current Directory {cwd}")
