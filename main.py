cb107dc857bc6125a571ae1b13a81c8d38ddaa68
Roye <ruthsus132@gmail.com>
2026-03-20 10:12:21 +0200

    added main.py, a simple menu tool for checking system information

---
+++ main.py
@@ -0,0 +1,18 @@
+while true: 
+ print("- [1] System Info")
+ print("- [2] Log Checker")
+ print("- [3] Task List")
+ print("- [0] Exit")
+ choice = int(input("select menu option"))
+ match choice
+ 
+  case 1: 
+  input ("Coming soon")
+  case 2:
+  input ("Coming soon")
+  case 3:
+  intput ("Coming soon")
+  case 0:
+  input ("press enter to exit")
+  break
+
